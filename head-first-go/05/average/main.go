907183f7beffc8c0a5721131f3d2539bdb8a63b4
j.lazerson <jake.lazerson@valuecentric.com>
2019-09-02 12:00:27 -0400

    BH - learning go - thank you Hashem

--- head-first-go/05/average/main.go
+++ head-first-go/05/average/main.go
@@ -3,19 +3,35 @@ package main
 
 import (
 	"fmt"
-	"github.com/headfirstgo/datafile"
 	"log"
+
+	"github.com/Ylazerson/go-shenanigans/head-first-go/05/datafile"
 )
 
+// THIS IS OBVIOUSLY NOT THE CORRECT APPRAOCH TO USE
+// ... but hey for now .....
+const filePath = "/home/laz/tmp/ch05-data.txt"
+
 func main() {
-	numbers, err := datafile.GetFloats("data.txt")
+
+	// -- --------------------------------------
+	numbers, err := datafile.GetFloats(filePath)
+
 	if err != nil {
 		log.Fatal(err)
 	}
+	// -- --------------------------------------
+
+	// -- --------------------------------------
+	// Get average:
 	var sum float64 = 0
+
 	for _, number := range numbers {
 		sum += number
 	}
+
 	sampleCount := float64(len(numbers))
+
 	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
+	// -- --------------------------------------
 }
