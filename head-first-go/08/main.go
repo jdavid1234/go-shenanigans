226790ec63d7ff04aa43502f8f83595074eaf9a4
j.lazerson <jake.lazerson@valuecentric.com>
2019-09-03 09:35:08 -0400

    BH - learning go - thank you Hashem

---
+++ head-first-go/08/main.go
@@ -0,0 +1,31 @@
+// B"H
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/Ylazerson/go-shenanigans/head-first-go/08/magazine"
+)
+
+func main() {
+	subscriber := magazine.Subscriber{Name: "Aman Singh"}
+	subscriber.Street = "123 Oak St"
+	subscriber.City = "Omaha"
+	subscriber.State = "NE"
+	subscriber.PostalCode = "68111"
+	fmt.Println("Street:", subscriber.Street)
+	fmt.Println("City:", subscriber.City)
+	fmt.Println("State:", subscriber.State)
+	fmt.Println("Postal Code:", subscriber.PostalCode)
+
+	employee := magazine.Employee{Name: "Joy Carr"}
+	employee.Street = "456 Elm St"
+	employee.City = "Portland"
+	employee.State = "OR"
+	employee.PostalCode = "97222"
+	fmt.Println("Street:", employee.Street)
+	fmt.Println("City:", employee.City)
+	fmt.Println("State:", employee.State)
+	fmt.Println("Postal Code:", employee.PostalCode)
+}
