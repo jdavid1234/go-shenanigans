6d1079557afd29b67fafda5e24642fadb85ee091
j.lazerson <jake.lazerson@valuecentric.com>
2019-09-03 08:51:20 -0400

    BH - learning go - thank you Hashem

--- head-first-go/06/average/main.go
+++ head-first-go/06/average/main.go
@@ -1,21 +1,42 @@
+// B"H
+
 // average calculates the average of several numbers.
 package main
 
 import (
 	"fmt"
-	"github.com/headfirstgo/datafile"
 	"log"
+
+	"github.com/Ylazerson/go-shenanigans/head-first-go/06/datafile"
 )
 
+// -- ------------------------------------------
+// THIS IS OBVIOUSLY NOT THE CORRECT APPRAOCH TO USE
+// ... but hey for now .....
+const filePath = "/home/laz/tmp/ch06-data.txt"
+
+// -- ------------------------------------------
+
 func main() {
-	numbers, err := datafile.GetFloats("data.txt")
+
+	// -- --------------------------------------
+	numbers, err := datafile.GetFloats(filePath)
+
 	if err != nil {
 		log.Fatal(err)
 	}
+	// -- --------------------------------------
+
+	// -- --------------------------------------
 	var sum float64 = 0
+
 	for _, number := range numbers {
 		sum += number
 	}
+
 	sampleCount := float64(len(numbers))
+
 	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
+	// -- --------------------------------------
+
 }
--- head-first-go/06/average2/main.go
+++ head-first-go/06/average2/main.go
@@ -1,4 +1,22 @@
-// average2 calculates the average of several numbers.
+// B"H
+
+/*
+average2 calculates the average of several numbers.
+
+Note, this is a command line tool. So `go install` it first.
+
+➜  06 git:(master) ✗ cd average2
+
+➜  average2 git:(master) ✗ go install
+
+➜  average2 git:(master) ✗ average2 12 23 34
+Average: 23.00
+
+➜  average2 git:(master) ✗ average2 12 23 34 45 99 9999999
+Average: 1666702.00
+
+*/
+
 package main
 
 import (
@@ -8,23 +26,40 @@ import (
 	"strconv"
 )
 
+// -- ------------------------------------------
+// Note this is a variadic function.
 func average(numbers ...float64) float64 {
 	var sum float64 = 0
+
 	for _, number := range numbers {
 		sum += number
 	}
+
 	return sum / float64(len(numbers))
 }
 
+// -- ------------------------------------------
+
 func main() {
+
+	// Get the command-line args:
 	arguments := os.Args[1:]
+
 	var numbers []float64
+
+	// -- --------------------------------------
 	for _, argument := range arguments {
+
 		number, err := strconv.ParseFloat(argument, 64)
+
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		numbers = append(numbers, number)
 	}
+	// -- --------------------------------------
+
+	// Note how we add `...` to convert the slice into "filling" the variadic parameter:
 	fmt.Printf("Average: %0.2f\n", average(numbers...))
 }
